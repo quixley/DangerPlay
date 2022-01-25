52a0f5361c2ec80c4a35ddaa1873f4a4dbddfe06
Artur Mikhaylov <v.artur.mikhaylov@reddit.com>
2022-01-25 18:29:25 +0300

    project created

---
+++ Shared/DangerPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerPlayApp.swift
+//  Shared
+//
+//  Created by Arthur Mikhaylov on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DangerPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
